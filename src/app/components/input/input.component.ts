d947fef57b148773dd73d627844a5ef95a959031
Jheff <79949084+Jefersonvitorpereira@users.noreply.github.com>
2025-05-14 14:59:34 -0300

    feat: passando dados entre componente pai e filho, emitir evento e editgar itens existentes na lista

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,13 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Item } from './../../interfaces/iItem';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, OnChanges {
+  @Input() ItemQueVaiSerEditado!: Item;
+  valorItem!: string;
+  editando: boolean = false;
+  textoBtn: string = 'Salvar item'
 
-  constructor() { }
+  constructor(private listaService: ListaDeCompraService) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges){
+    if(!changes['ItemQueVaiSerEditado'].firstChange){
+      this.editando = true;
+      this.textoBtn = 'Editar item';
+      this.valorItem = this.ItemQueVaiSerEditado?.nome;
+    }
+  }
+
+  adicionarItem() {
+    this.listaService.adicionarItemNaLista(this.valorItem);
+    this.limparCampo();
+  }
+
+  limparCampo() {
+    this.valorItem = '';
+  }
+
+  editarItem(){
+    this.listaService.editarItemDaLista(this.ItemQueVaiSerEditado, this.valorItem);
+    this.limparCampo();
+    this.editando = false;
+    this.textoBtn = "Salvar item"
+  }
 }
